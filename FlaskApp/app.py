78b72016fcfbeab1bf641d5702d650c4ab81c6b8
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-07 22:26:03 +0300

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
-from flask_ngrok import run_with_ngrok
+import os 
+
+from flask import Flask 
+
+app = Flask(__name__) 
 
 
-app = Flask(__name__)
-run_with_ngrok(app)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/") 
+def index(): 
+    return "Привет от приложения Flask" 
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__': 
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port)
