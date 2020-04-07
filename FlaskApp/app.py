93b360c47c180bac873ec8b060e3d628788f2db1
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-07 22:33:08 +0300

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,13 +1,12 @@
 import os 
-
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__) 
 
 
 @app.route("/") 
-def index(): 
-    return "Привет от приложения Flask" 
+def home():
+    return render_template("Osnova.html")
 
 
 if __name__ == '__main__': 
