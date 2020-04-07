f5f532236e660476b749da412151abeb434cb4f6
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-07 22:06:33 +0300

    Add files via upload

---
+++ FlaskApp/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_ngrok import run_with_ngrok
+
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route("/")
+def home():
+    return render_template("Osnova.html")
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
